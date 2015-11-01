d9e8a687f61131ca168ad9202103436adc69a3a5
azu <azuciao@gmail.com>
2015-11-01 22:15:47 +0900

    fix(npm): add main and bin

--- src/textlint-stat.js
+++
@@ -1,9 +0,0 @@
-// LICENSE : MIT
-"use strict";
-import {TextLintEngine} from "textlint";
-const engine = new TextLintEngine({
-    rulePaths: [__dirname + "/rules"]
-});
-// formatter
-var results = engine.executeOnFiles([__dirname + "/../README.md"]);
-console.log(results[0]);
